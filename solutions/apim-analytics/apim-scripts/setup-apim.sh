913dbcc8d041393197c5ada1606ec6bfdd2f7111
sajithd <sajithd@wso2.com>
2018-04-11 14:35:37 +0530

    Add APIM node configurations and scripts

--- solutions/apim-analytics/apim-scripts/setup-apim.sh
+++ solutions/apim-analytics/apim-scripts/setup-apim.sh
@@ -30,24 +30,18 @@ readonly DB_ENGINE=$8
 readonly DB_VERSION=${10}
 readonly DB_USERNAME=${12}
 readonly DB_PASSWORD=${14}
-readonly MY_NODE_ID=${16}
-readonly PRODUCT_NAME=${18}
-readonly PRODUCT_VERSION=${20}
-readonly DAS2_HOSTNAME=${22}
-readonly AWS_ACCESS_KEY_ID=${24}
-readonly AWS_ACCESS_KEY_SECRET=${26}
-readonly AWS_SECURITY_GROUP=${28}
-readonly AWS_REGION=${30}
+readonly PRODUCT_NAME=${16}
+readonly PRODUCT_VERSION=${18}
+readonly DAS1_IP=${20}
+readonly DAS2_IP=${22}
+
 readonly WUM_PRODUCT_NAME=${PRODUCT_NAME}-${PRODUCT_VERSION}
 readonly WUM_PRODUCT_DIR=/home/${USERNAME}/.wum-wso2/products/${PRODUCT_NAME}/${PRODUCT_VERSION}
 readonly INSTALLATION_DIR=/opt/wso2
 readonly PRODUCT_HOME="${INSTALLATION_DIR}/${PRODUCT_NAME}-${PRODUCT_VERSION}"
 
 # databases
-readonly WSO2_UM_DB="WSO2_UM_DB"
-readonly WSO2_REG_DB="WSO2_REG_DB"
-readonly WSO2_ANALYTICS_EVENT_STORE="WSO2_ANALYTICS_EVENT_STORE_DB"
-readonly WSO2_ANALYTICS_PROCESSED_DATA_STORE="WSO2_ANALYTICS_PROCESSED_DATA_STORE_DB"
+readonly WSO2_AM_DB="WSO2_AM_DB"
 readonly WSO2_AM_STATS_DB="WSO2_AM_STATS_DB"
 
 setup_wum_updated_pack() {
@@ -70,14 +64,14 @@ copy_libs() {
 copy_config_files() {
 
     echo ">> Copying configuration files "
-    cp -r -v /home/ubuntu/cloudformation-das/product-configs/* ${PRODUCT_HOME}/repository/conf/
+    cp -r -v /home/ubuntu/cloudformation-das/solutions/apim-analytics/product-apim-config/* ${PRODUCT_HOME}/repository/conf/
     echo ">> Done!"
 }
 
 copy_bin_files() {
 
     echo ">> Copying bin files "
-    cp -r -v /home/ubuntu/cloudformation-das/product-bin/* ${PRODUCT_HOME}/bin/
+    cp -r -v /home/ubuntu/cloudformation-das/solutions/apim-analytics/product-bin/* ${PRODUCT_HOME}/bin/
     echo ">> Done!"
 }
 
@@ -87,34 +81,21 @@ configure_product() {
     DAS_HOST_NAME=$(get_host_ip)
 
     echo ">> Configuring product "
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" -o -iname "*.sh" \) -print0 | xargs -0 sed -i 's/#_DAS_HOSTNAME_#/'$DAS_HOST_NAME'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" -o -iname "*.sh" \) -print0 | xargs -0 sed -i 's/#_DAS2_HOSTNAME_#/'$DAS2_HOSTNAME'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_AWS_ACCESS_KEY_ID_#/'$AWS_ACCESS_KEY_ID'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_AWS_ACCESS_KEY_SECRET_#/'$AWS_ACCESS_KEY_SECRET'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_AWS_SECURITY_GROUP_#/'$AWS_SECURITY_GROUP'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_AWS_REGION_#/'$AWS_REGION'/g'
+    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_DAS1_IP_#/'$DAS1_IP'/g'
+    find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_DAS2_IP_#/'$DAS2_IP'/g'
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_RDS_HOSTNAME_#/'$DB_HOST'/g'
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_RDS_PORT_#/'$DB_PORT'/g'
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_RDS_TYPE_#/'$DB_TYPE'/g'
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_DRIVER_CLASS_#/'$DRIVER_CLASS'/g'
     if [ $DB_ENGINE = "mysql" ]; then
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_UM_DB_#/'\\/$WSO2_UM_DB'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_REG_DB_#/'\\/$WSO2_REG_DB'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_ANALYTICS_PROCESSED_DATA_STORE_DB_#/'\\/$WSO2_ANALYTICS_PROCESSED_DATA_STORE'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_ANALYTICS_EVENT_STORE_DB_#/'\\/$WSO2_ANALYTICS_EVENT_STORE'/g'
+      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_AM_DB_#/'\\/$WSO2_AM_DB'/g'
       find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_AM_STATS_DB_#/'\\/$WSO2_AM_STATS_DB'/g'
     elif [ $DB_ENGINE = "sqlserver-ex" ]; then
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_UM_DB_#/'\;databaseName=$WSO2_UM_DB'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_REG_DB_#/'\;databaseName=$WSO2_REG_DB'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_ANALYTICS_PROCESSED_DATA_STORE_DB_#/'\;databaseName=$WSO2_ANALYTICS_PROCESSED_DATA_STORE'/g'
-      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_ANALYTICS_EVENT_STORE_DB_#/'\;databaseName=$WSO2_ANALYTICS_EVENT_STORE'/g'
+      find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_AM_DB_#/'\;databaseName=$WSO2_AM_DB'/g'
       find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_WSO2_AM_STATS_DB_#/'\;databaseName=$WSO2_AM_STATS_DB'/g'
     fi
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_DB_USER_#/'$DB_USERNAME'/g'
     find ${PRODUCT_HOME}/ -type f \( -iname "*.properties" -o -iname "*.xml" \) -print0 | xargs -0 sed -i 's/#_DB_PWD_#/'$DB_PASSWORD'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.dat" \) -print0 | xargs -0 sed -i 's/#_MY_NODE_ID_#/'$MY_NODE_ID'/g'
-    find ${PRODUCT_HOME}/ -type f \( -iname "*.conf" \) -print0 | xargs -0 sed -i 's/#_PRODUCT_HOME_#/'${PRODUCT_NAME}-${PRODUCT_VERSION}'/g'
-
     echo "Done!"
 }
 
@@ -158,7 +139,7 @@ get_jdbc_url_prefix() {
 
 start_product() {
     chown -R ${USERNAME} ${PRODUCT_HOME}
-    echo ">> Starting WSO2 DAS Server ... "
+    echo ">> Starting WSO2 APIM Server ... "
     sudo -u ${USERNAME} bash ${PRODUCT_HOME}/bin/wso2server.sh start
 }
 
